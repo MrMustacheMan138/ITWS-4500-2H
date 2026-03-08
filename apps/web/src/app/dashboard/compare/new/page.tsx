62b6a4734ef1a0cf358eb345b0e0e6df1dcdc96c
Jason-Z1 <zhengj12@rpi.edu>
2026-03-07 23:28:53 -0500

    Created the structure for side-bar + base set-up for the compare and dashboard pages

--- apps/web/src/app/dashboard/compare/new/page.tsx
+++ apps/web/src/app/dashboard/compare/new/page.tsx
@@ -1,1 +1,20 @@
 // New Comparison page
+import Header from "@/components/common/header"
+import Sidebar from "@/components/common/sidebar"
+
+export default function ComparePage () {
+   return (
+      <div className="flex min-h-screen">
+         <Sidebar />
+      
+         <div className="flex-1 ml-64 flex flex-col">
+            <Header />
+            
+            <main className="flex-1 p-6">
+               {/* COMPARE FORM WILL BE IMPORTED HERE */}
+               <h1>Compare page</h1>
+            </main>
+         </div>
+      </div>
+   );
+}
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Landing() {
 
             <div>
                <Link
-                  href=""
+                  href="/dashboard/compare/new"
                   className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded inline-block"
                > 
                   Start Comparing →
